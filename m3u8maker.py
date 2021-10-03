0f014aedb0209b4abb71c833f00a2a6f2f680d77
chekke1999 <nevec555@gmail.com>
2021-10-03 21:10:08 +0900

    あとは書き込むだけだが、カレント以外が基準の相対パスを吐き出すので再生時間が取得できなくなった

--- m3u8maker.py
+++ m3u8maker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import glob,os,sys,argparse,inspect
+import glob,os,sys,argparse,inspect,re
+from re import A
 sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 from pprint import pprint
 from lib.mutagen import File
@@ -7,28 +8,40 @@ from lib.pathlib import Path
 from lib.pathlib import PurePath
 
 def Playlist(args):
-    # プレイリストの保存先&ファイル名と、対象ディレクトリ
+    # プレイリストの保存先&ファイル名ジェネレータを返すジェネレータ
     playlist_dict = {}
     sep = os.sep
     def set_palylist(dirlib):
+        # 保存先
         playlist_dict["s_path"] = f"{args.save}{sep}{dirlib.name}.m3u8" if args.save != None else f"{dirlib.resolve()}.m3u8"
-        playlist_dict["f_list_gen"] = (j for j in (str(i) for i in dirlib.glob("**/*") if i.is_file()) if File(j) != None)
+        # ファイルリストジェネレーター
+        def flie_list_gen(): 
+            for k in (str(j) for j in (i for i in dirlib.glob("**/*") if i.is_file()) if File(str(j)) != None):
+                # デフォトは相対パス
+                if args.absolute_path:
+                    yield k
+                else:
+                    yield os.path.relpath(k,os.path.dirname(playlist_dict["s_path"]))
+        playlist_dict["f_list"] = flie_list_gen()
         return playlist_dict
     for dpath in args.input:
         # Namespace(input=['/hoge/hoge1'], sub_directory=False, absolute_path=False, save=None) 
         dirlib = Path(dpath)
         # 入力されたディレクトリ存在チェック
         if not dirlib.is_dir():
-            print(f"inputに指定したディレクトリ: {dpath}は存在しません。\n処理を中断します。")
-            exit()
+            print(f"inputに指定したディレクトリ: {dpath}は存在しません。")
+            continue
         if args.sub_directory:
-            # サブディレクトリ有効時
-            for dirlib_in in [i for i in dirlib.glob("*") if i.is_dir()]:
+            # サブディレクトリ有効時 and ディレクトリか否かのチェック
+            for dirlib_in in (i for i in dirlib.glob("*") if i.is_dir()):
                 yield set_palylist(dirlib_in)
         else:
             # サブディレクトリ無効時
             yield set_palylist(dirlib)
 
+def PlaylistWrite(playlist_dict):
+    # with opne(playlist_dict["s_path"]):
+    #     for file_path in playlist_dict["f_gen"]:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="ディレクトリをベースにm3u8形式のプレイリストを生成します。",formatter_class=argparse.RawTextHelpFormatter)
@@ -48,5 +61,5 @@ if __name__ == "__main__":
     のコマンドであることを明示的に示すことで、サーバのディレクトリパスに変換する")
     args = parser.parse_args()
     for i in Playlist(args):
-        pprint(i)
+        PlaylistWrite(i)
 
